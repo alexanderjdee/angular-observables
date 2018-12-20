a056ab96d2758715be1a0876ed66590a390a856c
alexander.dee <alexander.dee@bisimulations.com>
2018-12-20 10:57:41 -0500

    Added observables and fixed rxjs imports

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { interval, Subscription, Observable, Observer } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numbersObservableSubscription: Subscription;
+  customObservableSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    const myNumbers = interval(1000).pipe(map((data: number) => {
+      return data *3;
+    }));
+    
+    this.numbersObservableSubscription = myNumbers.subscribe((number: number) => {
+      console.log(number);
+    });
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+
+      setTimeout(() => {
+        observer.complete();
+      }, 5000);
+
+      setTimeout(() => {
+        observer.next('third package');
+      }, 6000);
+    });
+
+    this.customObservableSubscription = myObservable.subscribe(
+      (data: string) => { console.log(data); },
+      (error: string) => { console.log(error); },
+      () => { console.log('completed'); }
+    );
+  }
+
+  //Unsubscribe to avoid memory leak
+  ngOnDestroy() {
+    this.numbersObservableSubscription.unsubscribe();
+    this.customObservableSubscription.unsubscribe();
   }
 
 }
